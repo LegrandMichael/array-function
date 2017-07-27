8c06542a7de8a2cf9fb55e31646af44cbac03f16
LegrandMichael <michael.c.legrand@gmail.com>
2017-07-27 22:44:00 +0200

    fonction nombreHomme

--- script.js
+++ script.js
@@ -16,3 +16,22 @@ var classe2 = [
 { nom: 'Jacques', age: 35, sexe: 'M' },
 { nom: 'Eric', age: 47, sexe: 'M' }
 ];
+
+function nombreHomme (classe) {
+
+	var nbH = 0;
+	
+
+	for (var i = 0; i < classe.length; i++) {
+		if ( "M" === classe[i].sexe) {
+			nbH++;
+		}
+	}
+	return nbH;
+};
+
+var nbHomme = nombreHomme(classe1);
+console.log("le nombre d'hommes de la classe 1 est de " + nbHomme);
+
+var nbHomme = nombreHomme(classe2);
+console.log("le nombre d'hommes de la classe 2 est de " + nbHomme);
