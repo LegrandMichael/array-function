a809fa538d1ebc080b128f5bda2a550f9edbbb9c
LegrandMichael <michael.c.legrand@gmail.com>
2017-07-27 14:22:31 +0200

    Step 1.1

---
+++ script.js
@@ -0,0 +1,18 @@
+var classe1 = [
+{ nom: 'Pierre', age: 40, sexe: 'M' },
+{ nom: 'Marie', age: 18, sexe: 'F' },
+{ nom: 'Louise', age: 16 , sexe: 'F' },
+{ nom: 'Damien', age: 55, sexe: 'M' },
+{ nom: 'Louis', age: 19, sexe: 'M' }
+];
+
+var classe2 = [
+{ nom: 'Elodie', age: 27, sexe: 'F' },
+{ nom: 'Christophe', age: 48, sexe: 'M' },
+{ nom: 'Aurelie', age: 32, sexe: 'F' },
+{ nom: 'Jeanne', age: 15, sexe: 'F' },
+{ nom: 'Martin', age: 77, sexe: 'M' },
+{ nom: 'Eric', age: 18, sexe: 'M' },
+{ nom: 'Jacques', age: 35, sexe: 'M' },
+{ nom: 'Eric', age: 47, sexe: 'M' }
+];
