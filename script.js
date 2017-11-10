08d871f743a72859845267997fca366bc21f260f
LegrandMichael <michael.c.legrand@gmail.com>
2017-11-10 11:36:03 +0100

    Ajout fonction proportionHomme

--- script.js
+++ script.js
@@ -30,8 +30,58 @@ function nombreHomme (classe) {
 	return nbH;
 };
 
-var nbHomme = nombreHomme(classe1);
-console.log("le nombre d'hommes de la classe 1 est de " + nbHomme);
+function proportionHomme(classe){
+	var nbH = 0;
 
-var nbHomme = nombreHomme(classe2);
-console.log("le nombre d'hommes de la classe 2 est de " + nbHomme);
+	for (var i = 0; i < classe.length; i++) {
+		if ( "M" === classe[i].sexe) {
+			nbH++;
+		}
+	}
+	nbH = (nbH/classe.length)*100+"%";
+	return nbH;
+
+};
+
+
+function proportionFemme(classe){
+	var nbF = 0;
+
+	for (var i = 0; i < classe.length; i++) {
+		if ( "F" === classe[i].sexe) {
+			nbF++;
+		}
+	}
+	nbF = (nbF/classe.length)*100+"%";
+	return nbF;
+
+};
+
+// 1.6. Modifiez la fonction précédente pour quelle puisse prendre en compte le sexe: 
+// moyenneAge( classe, sexe = 'T'). L'argument sexe aura une valeur par défaut 'T' (pour Tous) 
+// ainsi si l'argument est omis lors de l'appel de la fonction nous ferons une moyenne d'age sur 
+// tous si non sur le sexe précisé. Prévoyé un message d'erreur si la valeur passé à l'argument 
+// sexe est différente de T,M,F.
+
+function moyenneAge(classe, sexe = "T") {
+	var age = 0;
+	for (var i = 0; i < classe.length;i++) {
+		age += classe[i].age;
+	}
+	age = age/classe.length;
+	return age;
+};
+
+
+// 1.7. Définissez une fonction plusVieux( classe ) qui renvoie l'objet associé à la personne la plus vielle 
+// ( On part du principe que toutes les personnes ont un age différent)
+
+
+
+
+console.log("Dans la classe 1 il y a "+nombreHomme(classe1)+" hommes, soit " + proportionHomme(classe1)+" et "+ proportionFemme(classe1)+" de femmes");
+console.log("Dans la classe 2 il y a " +nombreHomme(classe2)+" hommes, soit "+ proportionHomme(classe2)+" et "+ proportionFemme(classe2)+" de femmes");
+
+console.log("La moyenne d'âge de la classe 1 est de " + Math.floor(moyenneAge(classe1)) + " ans");
+console.log("La moyenne d'âge de la classe 2 est de " + Math.floor(moyenneAge(classe2)) + " ans");
+
