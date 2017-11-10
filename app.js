b1065420570dce54734efc06a72af0e5f59863c5
LegrandMichael <michael.c.legrand@gmail.com>
2017-11-10 11:35:25 +0100

    Création du tableau par fonction

---
+++ app.js
@@ -0,0 +1,37 @@
+function createRow (j, nbCol) {
+  var html = "";
+  for (var i = 0; i < nbCol;i++) {
+     html +="<td>";
+     html +=(j+1);
+     html +="x";
+     html +=(i+1);
+     html +="=";
+     html += ((j+1)*(i+1));
+     html +="</td>";
+    }
+  return html;
+}
+
+function createTable(nbLi, nbCol) {
+              
+
+// Intialisation de la table
+var html1 = "<table>";
+
+
+// boucle de création des colonnes
+for (var j = 0; j < nbLi;j++) {
+     html1 += "<tr>";
+     html1 += createRow(j,nbCol);
+     html1 += "</tr>";
+}
+
+// finalisation 
+html1 += "</tr></table>";
+
+return html1;
+  
+}
+
+$("#container").html(createTable(10, 10));
+
